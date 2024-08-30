23c19bcc2b60b0961b197cf9283b4ed0ba093a2a
Olivier Guilbault <2250637@cegepmontpetit.ca>
2024-08-30 17:32:53 -0400

    FCT labo 3

---
+++ Migrations/20240830203600_genreOptionnel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationsNaN.Migrations
+{
+    /// <inheritdoc />
+    public partial class genreOptionnel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Game_Genre_GenreId",
+                table: "Game");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Game",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Game_Genre_GenreId",
+                table: "Game",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Game_Genre_GenreId",
+                table: "Game");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Game",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Game_Genre_GenreId",
+                table: "Game",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
